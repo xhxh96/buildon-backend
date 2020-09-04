1cbf15ad66670d6d2ee749d5220ee82393e4c210
xhxh96 <xianhao@u.nus.edu>
2020-09-04 17:13:35 +0800

    Added tastebreaker and recommended stocks

--- routes/stocks.js
+++ routes/stocks.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 const router = express.Router();
 
 router.get('/topFive', async function (req, res) {
-  const stocks = ['AAPL', 'BABA', 'TSLA', 'GOOGL', 'GM']
+  const stocks = ['GS', 'MDT', 'NAT', 'CRK', 'SID']
   const params = {
     access_key: apiKey
   }
@@ -27,6 +27,40 @@ router.get('/userStock', async function (req, res) {
   const { email } = req.body
 
   const stocks = email === 'peter@email.com' ? ['GOOGL', 'TRIL', 'OSMT', 'KPTI', 'PHAS'] : ['SMH', 'GLUU', 'XLU', 'SRET', 'EXAS']
+  const params = {
+    access_key: apiKey
+  }
+  Promise.all(stocks.map(stock => axios.get(`http://api.marketstack.com/v1/tickers/${stock}/eod/latest`,
+    {params})
+    .then(response => response.data)))
+    .then(value => {
+      const response = []
+      for (let i = 0; i < stocks.length; i++) {
+        response.push({stock: stocks[i], data: value[i]})
+      }
+      res.status(200).json(response)
+    }).catch(err => res.status(400).json(err.message))
+})
+
+router.get('/userStockRecommended', async function (req, res) {
+  const stocks = ['LQD', 'IBDL', 'TOTL', 'USHY']
+  const params = {
+    access_key: apiKey
+  }
+  Promise.all(stocks.map(stock => axios.get(`http://api.marketstack.com/v1/tickers/${stock}/eod/latest`,
+    {params})
+    .then(response => response.data)))
+    .then(value => {
+      const response = []
+      for (let i = 0; i < stocks.length; i++) {
+        response.push({stock: stocks[i], data: value[i]})
+      }
+      res.status(200).json(response)
+    }).catch(err => res.status(400).json(err.message))
+})
+
+router.get('/userStockTasteBreaker', async function (req, res) {
+  const stocks = ['MSFT', 'DD', 'CALA', 'ACAD', 'CRNC']
   const params = {
     access_key: apiKey
   }
